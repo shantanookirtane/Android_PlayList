d3075d4efc729115f4ab9cbcbcafaa7646e17cde
shantanookirtane@gmail.com <shantanookirtane@gmail.com>
2013-11-16 15:52:40 -0500

    Added code for localstorage enable and change the name of the html file

--- Android_PlayList/src/com/example/android_playlist/MainActivity.java
+++ Android_PlayList/src/com/example/android_playlist/MainActivity.java
@@ -3,8 +3,8 @@ package com.example.android_playlist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
@@ -13,8 +13,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.activity_main);
         WebView webView = new WebView(this);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/index.html");
+        WebSettings settings = webView.getSettings();
+        
+        // TO enable JS
+        settings.setJavaScriptEnabled(true);
+        // To enable Localstorage
+        settings.setDomStorageEnabled(true);
+        
+        webView.loadUrl("file:///android_asset/main.html");
         setContentView(webView);
         /*By default Javascript is turned off,
          * it can be enabled by this line.
